a2c31e8d5ee12c884b7ccaa4ccc80c5b3656ddbf
Raghav Singhal <84004096+raghavsinghalrs@users.noreply.github.com>
2023-05-05 10:24:57 +0530

    Insert and search nodes in a BST

---
+++ main.py
@@ -0,0 +1,41 @@
+class BST:
+  
+  def __init__(self,val):
+    self.val = val
+    self.left = None
+    self.right = None
+    
+  def insert(self,val):
+    if self.val is None:
+      self.val = val 
+      return
+
+    if self.val>val:
+      if self.left:
+        self.left.insert(val)
+      else:
+        self.left = BST(val)
+    else:
+      if self.right:
+        self.right.insert(val)
+      else:
+        self.right =BST(val)
+  def search(self,val):
+    if self.val==val:
+      print('You got your node!')
+      return
+    if self.val>val:
+      if self.left:
+        self.left.search(val)
+      else:
+        print('Sorry, Data is not exist!')
+    else:
+      if self.right:
+        self.right.search(val)
+      else:
+        print('Sorry, Data is not exist!')
+root = BST(10)
+lst = [20,4,9,5,7,22,3]
+for i in lst:
+  root.insert(i)
+root.search(9)
